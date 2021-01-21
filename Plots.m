96799ceb7699abc75d5cf8d0e8ecf3cf20ccc86c
lzx <lzx12138@sjtu.edu.cn>
2021-01-21 13:29:10 +0800

    Upload graphs with legends

--- Plots.m
+++ Plots.m
@@ -11,6 +11,7 @@ load(strcat('counts_', num2str(n)));
 total_max = 1;
 len = size(counts, 2);
 % x = 0 : len - 1;
+figure(1);
 for i = 1:n + 1
     local_max = 1;
     for j = 1:len
@@ -26,16 +27,29 @@ for i = 1:n + 1
     subplot(xLen, yLen, i + 1);
     plot(x, counts(i, 1:local_max));
     
-    title(num2str(i - 1));
-    % xlabel('changes');
-    % ylabel('counts');
+    title(strcat(num2str(i - 1), ' bit switch'));
+    xlabel('num of wires flipped');
+    ylabel('occurence');
 end
 
 x = 0 : total_max - 1;
 subplot(xLen, yLen, 1);
-% figure(2);
 hold on;
 for i = 1:n + 1
-    plot(x, counts(i, 1:total_max));
+    plot(x, counts(i, 1:total_max), 'Displayname', strcat(num2str(i - 1), ' switch'));
 end
 title('total');
+hold off;
+lgd = legend;
+lgd.NumColumns = 2;
+
+
+figure(2);
+hold on;
+for i = 1:n + 1
+    plot(x, counts(i, 1:total_max), 'Displayname', strcat(num2str(i - 1), ' switch'));
+end
+title('total');
+hold off;
+lgd = legend;
+lgd.NumColumns = 2;
